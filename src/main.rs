1fe816f455d19d8ed6251dd8b4a044901c640674
DESKTOP-LDRG8J8\Tapok <Tapok134224@mail.ru>
2023-08-23 20:20:08 +0300

    feature: add docker async

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
 pub mod models;
 pub mod schema;
-use std::sync::Arc;
 
 use actix_web::{
-    web::{self, Data},
+    web::{self},
     App, HttpServer,
 };
-use diesel_async::{pooled_connection::{deadpool::Pool, AsyncDieselConnectionManager}, AsyncPgConnection};
+use diesel_async::pooled_connection::{deadpool::Pool, AsyncDieselConnectionManager};
 use dotenv::dotenv;
-
-pub struct AppState {
-    db:  Arc<Pool<AsyncPgConnection>>,
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -20,15 +15,18 @@ async fn main() -> std::io::Result<()> {
 
     let config = AsyncDieselConnectionManager::<diesel_async::AsyncPgConnection>::new(database_url);
     let pool = Pool::builder(config)
+        .max_size(10)
         .build()
         .expect("error building connection");
 
     // checkout a connection from the pool
-    let mut conn = pool.get().await.expect("error creating connection");
+    // let mut conn = pool.get().await.expect("error creating connection");
 
-    HttpServer::new(|| App::new().service(web::scope("api/v1").app_data(AppState{db: Arc::new(pool.clone())})))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new().app_data(web::Data::new(pool.clone()))
+        // .route("/{name}", web::get().to(index))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
-// let mut connection = AsyncPgConnection::establish(&std::env::var("DATABASE_URL")?).await?;
